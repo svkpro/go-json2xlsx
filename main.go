3ddb5e2ca50923892c8ce16844307c3c2e143040
Vasili Solovei <gsvkpro@gmail.com>
2020-01-26 22:00:37 +0300

    Added a simple service to count the of passed string

--- main.go
+++ main.go
@@ -1,15 +1,80 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
+	"errors"
+	"github.com/go-kit/kit/endpoint"
+	httptransport "github.com/go-kit/kit/transport/http"
 	"log"
 	"net/http"
 )
 
-func main() {
-	http.HandleFunc("/healthcheck", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte (http.StatusText(http.StatusOK)))
-		w.WriteHeader(http.StatusOK)
-	})
+type StringService interface {
+	Count(string) (int, error)
+}
 
-	log.Fatal(http.ListenAndServe(":9999", nil))
+type stringService struct{}
+
+func (stringService) Count(s string) (int, error) {
+	if s == "" {
+		return 0, errors.New("The string can't be empty!")
+	}
+	return len(s), nil
+}
+
+type countRequest struct {
+	String string `json:"string"`
+}
+
+type countResponse struct {
+	Amount int `json:"amount"`
+}
+
+func makeCountEndpoint(svc StringService) endpoint.Endpoint {
+	return func(_ context.Context, request interface{}) (interface{}, error) {
+		req := request.(countRequest)
+		amount, err := svc.Count(req.String)
+
+		return countResponse{amount}, err
+	}
+}
+
+func main() {
+	svc := stringService{}
+	opts := []httptransport.ServerOption{
+		httptransport.ServerErrorEncoder(encodeError),
+	}
+
+	countHandler := httptransport.NewServer(
+		makeCountEndpoint(svc),
+		decodeCountRequest,
+		encodeResponse,
+		opts...,
+	)
+	http.Handle("/count", countHandler)
+	log.Fatal(http.ListenAndServe(":9090", nil))
+}
+
+func decodeCountRequest(_ context.Context, r *http.Request) (interface{}, error) {
+	var request countRequest
+	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+		return nil, err
+	}
+
+	return request, nil
+}
+
+func encodeResponse(ctx context.Context, w http.ResponseWriter, response interface{}) error {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+
+	return json.NewEncoder(w).Encode(response)
+}
+
+func encodeError(_ context.Context, err error, w http.ResponseWriter) {
+	w.WriteHeader(http.StatusBadRequest)
+
+	json.NewEncoder(w).Encode(map[string]interface{}{
+		"error": err.Error(),
+	})
 }
